9f3182321b6c741b9f61d33d8281cb4351ce8959
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-01-13 17:15:56 -0600

    Pseudocoded 03.

---
+++ 03/03.php
@@ -0,0 +1,35 @@
+<?php
+
+// __Challenge 3__:
+// Write a script that prints a list of all of the DNS domains on an account. Let the user select a domain from the list and add an "A" record to that domain by entering an IP Address TTL, and requested "A" record text. This must be done in PHP with php-opencloud. 
+
+
+// Print all domains in DNS for this account
+
+// Loop
+// Prompt for user input - a domain
+// Validate input - numeric option or ctype_alnum+'.'
+// Verify domain entered exists
+// End loop
+
+
+// Loop
+// Prompt for subdomain name - [input].rootmypc.net
+// Validate ctype_alnum
+// End loop
+
+// Loop
+// Prompt for TTL in seconds
+// Validate ctype_digit
+// End loop
+
+// Loop
+// Prompt for IP address
+// Verify valid IP
+// End loop
+
+
+// Create A record
+
+
+?>
---
+++ 04/04.php
@@ -0,0 +1,10 @@
+<?php
+
+// __Challenge 4__:
+// Write a script that creates a Cloud Files Container. If the container already exists, exit and let the user know. The script should also upload a directory from the local filesystem to the new container, and enable CDN for the new container. The script must return the CDN URL. This must be done in PHP with php-opencloud. 
+
+
+
+
+
+?>
