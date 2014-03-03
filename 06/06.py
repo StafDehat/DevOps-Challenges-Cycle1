ec2a774c864a666f69ca56bff9de13b202995d88
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-03-03 17:18:53 -0600

    Undeclared

--- 06/06.py
+++ 06/06.py
@@ -9,3 +9,61 @@
 # language and SDK! 
 
 
+# Instance, Database, User, Password?
+
+import sys
+import getopt
+import uuid
+import time
+import pprint
+import argparse
+import time
+from auth import *
+
+CDB = pyrax.cloud_databases
+
+
+parser = argparse.ArgumentParser(description='Backup a CDB database:')
+parser.add_argument('-i','--instance', help='CDB Instance',required=True)
+parser.add_argument('-d','--database',help='CDB Database', required=True)
+parser.add_argument('-u','--user',help='CDB User with access to database', required=True)
+args = parser.parse_args()
+ 
+## show values ##
+print ("Instance: %s" % args.instance )
+print ("Database: %s" % args.database )
+print ("User:     %s\n" % args.user )
+
+#
+# Ensure CDB instance exists
+exists = False
+cdbs = CDB.list()
+for cdb in cdbs:
+  if cdb.name == args.instance:
+    print "CDB instance '" + cdb.name.rstrip() + "' exists\n"
+    exists = True
+    break
+  #fi 
+#done
+if not exists:
+  print "CDB instance '" + args.instance + "' does not exist\n"
+  sys.exit()
+#fi
+
+
+#
+# Ensure named database exists
+# Nevermind, we don't need the database.
+
+
+#
+# Ensure named user exists
+# Nevermind, we don't need the user.
+
+
+print "Thanks for providing a database and user, but I'm gonna disregard that info.\n"
+backupname=args.instance + "." + time.strftime("%F.%T")
+print "Creating backup of instance '" + args.instance + "' named '" + backupname + "'"
+cdb.create_backup(backupname)
+print "That'll be done soon, thanks.\n"
+
--- 07/07.py
+++ 07/07.py
@@ -6,3 +6,64 @@
 # bonus point, add an Error page served via the Load Balancer for when none of 
 # your nodes are available. Choose your language and SDK!
 
+import os
+import pyrax
+import pprint
+
+CS = pyrax.cloudservers
+CLB = pyrax.cloud_loadbalancers
+namebase = "andr4596"
+numnodes = 2
+
+# Set csimage
+for img in CS.images.list():
+  if "CentOS 6.4" in img.name:
+    csimage = img
+    break
+  #fi
+#done
+
+# Set cssize
+for flavor in CS.flavors.list():
+  if flavor.ram == 1024:
+    cssize = flavor
+    break
+  #fi
+#done
+
+#
+# Initialize the builds
+servers = []
+for x in range(1,numnodes+1):
+  servername = namebase + "-0" + str(x)
+  servers.append(CS.servers.create(servername, csimage.id, cssize.id))
+#done
+
+#
+# Wait for server builds to finish
+for server in servers:
+  while True:
+    server = CS.servers.get(server.id)
+    time.sleep(10)
+    print "Waiting on " + server.name + "..."
+    if server.status != "ACTIVE":
+      break
+    #fi
+  #done
+#done
+
+pprint.pprint(server)
+
+#
+# Build a load balancer with the cloud servers as nodes
+nodes = []
+for server in servers:
+  node = clb.Node(address=server., port=80, condition="ENABLED")
+  vip = clb.VirtualIP(type="PUBLIC")
+  lb = clb.create(lb_name, port=80, protocol="HTTP", nodes=[node], virtual_ips=[vip])
+#done
+
+
+
+
+
