06711d894c322277c97e388d75bf9e58c9cf0dea
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-03-20 10:38:15 -0500

    Whoops, cd was out of order

--- push.sh
+++ push.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+cd /home/ahoward/DevOps-Challenges-Cycle1
 git pull
 git status | grep deleted | perl -pe 's/^#\s*deleted:\s*(.*)$/\1/' | \
 while read LINE; do
   git rm $LINE
 done
-cd /home/ahoward/DevOps-Challenges-Cycle1
 find . | xargs git add
 git commit
 git push
