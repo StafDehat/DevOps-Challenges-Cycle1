28a14b0ad67859564277a4858d06efd4ebbcd875
Andrew Howard <ahoward@phoenix.rootmypc.net>
2014-03-20 10:34:54 -0500

    Added deletes to push.sh

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+git pull
+git status | grep deleted | perl -pe 's/^#\s*deleted:\s*(.*)$/\1/' | \
+while read LINE; do
+  git rm $LINE
+done
 cd /home/ahoward/DevOps-Challenges-Cycle1
 find . | xargs git add
 git commit
